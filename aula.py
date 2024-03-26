8e1017719798a4f5c08e286555d116f4cb31c51f
mahiromiko <lucas25th@gmail.com>
2024-03-26 20:30:06 -0300

    outro

--- aula.py
+++ aula.py
@@ -18,3 +18,26 @@ type(altura)
 dev = True
 type(dev)
 
+idade1 = 20
+idade2 = 35
+
+soma = idade1 + idade2
+print(soma)
+
+mult = idade1 * idade2
+print(mult)
+
+div = idade1 / idade2
+print(div)
+
+sub = idade1 - idade2
+print(sub)
+
+print(15 ** 2)
+
+#só a parte inteira
+print(idade2 // idade1)
+
+#pega o resto (modulo)
+print(idade2 % idade1)
+

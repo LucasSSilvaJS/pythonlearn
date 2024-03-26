eb8a06297b5de0a408453bcb9392ba2bd58c16d1
mahiromiko <lucas25th@gmail.com>
2024-03-26 19:54:21 -0300

    publicar novamente

---
+++ aula.py
@@ -0,0 +1,20 @@
+print('hello world')
+
+pessoa = 'Lucas'
+print(pessoa)
+
+idade = 37
+print(idade)
+
+pessoa = 'Lucas'
+type(pessoa)
+
+idade = 25
+type(idade)
+
+altura = 1.75
+type(altura)
+
+dev = True
+type(dev)
+
